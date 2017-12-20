3ed165530dcdd9216eb580e7b637754b5ccff356
徐丹丹 <eggyang@xudandandeMBP.lan>
2017-12-20 09:55:45 +0800

    保存

--- js/home.js
+++ js/home.js
@@ -2,21 +2,22 @@
 
     $(function () {
 
-        $(".title-ul li").click(function () {
-            console.log($(this).index());
+        $(".title-ul li").click(function (e) {
             for (var i = 0; i < $(".title-ul li").length; i++) {
                 $(".title-ul li").removeClass("active");
             }
             $(".title-ul li").eq($(this).index()).addClass("active");
+            getContent($(this).attr("codes"));
         });
 
 
+        getContent("page001");
 
-
-
-
-
-
+        function getContent(codes) {
+            $.get("./html/" + codes + ".html", function (data) {
+                $(".content").html(data);
+            });
+        }
 
 
 
